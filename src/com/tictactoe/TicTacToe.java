f62a4dd9c19d4d742a9256a94e29c5240d33b215
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-28 12:02:11 +0530

    [Devesh]: Added Board for Tic Tac Toe Game

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -2,5 +2,11 @@ package com.tictactoe;
 
 public class TicTacToe {
 	
-		
+	public Character[] createBoard() {
+		Character[] charArr = new Character[10];
+		for(int i=1;i<charArr.length;i++) {
+			charArr[i]='_';
+		}
+		return charArr;
 	}
+}
--- src/com/tictactoe/TicTacToeRunner.java
+++ src/com/tictactoe/TicTacToeRunner.java
@@ -1,5 +1,8 @@
 package com.tictactoe;
 
 public class TicTacToeRunner {
-
+	public static void main(String[] args) {
+		TicTacToe ticTacToeGame = new TicTacToe();
+		Character[] board = ticTacToeGame.createBoard();
+	}
 }
