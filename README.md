b9604b59f24167010a6ba6abd98374557746662d
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-28 11:59:03 +0530

    [Devesh]: Added Tic Tac Toe basic setup

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Welcome to Tic Tac Toe Game</h1>
---
+++ src/com/tictactoe/TicTocToeRunner.java
@@ -0,0 +1,8 @@
+package com.tictactoe;
+
+public class TicTocToeRunner {
+	public static void main(String[] args) {
+		TicTacToe ticTacToeGame = new TicTacToe();
+		ticTacToeGame.createBoard();
+	}
+}
