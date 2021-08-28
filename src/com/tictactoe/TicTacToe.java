e4a3487ff9a346e1fe42e26890e23d57bbf20112
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-28 12:25:21 +0530

    [Delete]: 2 Runner error solved

--- src/com/tictactoe/TicTacToe.java
+++ src/com/tictactoe/TicTacToe.java
@@ -12,16 +12,6 @@ public class TicTacToe {
 		return charArr;
 	}
 	
-	public char chooseXorO() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose X or O");
-		char choose = sc.nextLine().charAt(0);
-		return Character.toUpperCase(choose);
-	}
+	
 
-	public void showBoard(Character[] charArr) {
-		for(int i=1;i<charArr.length;i=i+3) {
-			System.out.println(charArr[i]+ " "+charArr[i+1]+ " "+charArr[i+2] );
-		}
-	}
 }
--- src/com/tictactoe/TicTacToeRunner.java
+++ src/com/tictactoe/TicTacToeRunner.java
@@ -4,9 +4,6 @@ public class TicTacToeRunner {
 	public static void main(String[] args) {
 		TicTacToe ticTacToeGame = new TicTacToe();
 		Character[] board = ticTacToeGame.createBoard();
-		char player = ticTacToeGame.chooseXorO();
-		char computer = ( player == 'X')? 'O':'X';
-		System.out.println("Player: "+player+" Computer: "+computer);
-		ticTacToeGame.showBoard(board);
+		
 	}
 }
