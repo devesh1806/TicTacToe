39bf90d97f0e59690860982e85e4b488421588f7
Devesh Bhangale <42887196+devesh1806@users.noreply.github.com>
2021-08-28 12:22:35 +0530

    [Delete]: 2 Runner error solved

--- src/com/tictactoe/TicTocToeRunner.java
+++
@@ -1,8 +0,0 @@
-package com.tictactoe;
-
-public class TicTocToeRunner {
-	public static void main(String[] args) {
-		TicTacToe ticTacToeGame = new TicTacToe();
-		ticTacToeGame.createBoard();
-	}
-}
