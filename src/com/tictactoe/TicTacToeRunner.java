d01f28f70e068ee1bed94ddb7053eda5476b05f4
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-30 18:46:13 +0530

    [Devesh]: Added ability to continue playing new game.

--- src/com/tictactoe/TicTacToeRunner.java
+++ src/com/tictactoe/TicTacToeRunner.java
@@ -1,45 +1,58 @@
 package com.tictactoe;
+import java.util.Scanner;
 
 public class TicTacToeRunner {
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
 		TicTacToe ticTacToeGame = new TicTacToe();
-		Character[] board = ticTacToeGame.createBoard();
-		char player = ticTacToeGame.chooseXorO();
-		char computer = ( player == 'X')? 'O':'X';
-		System.out.println("Player: "+player+" Computer: "+computer);
-		ticTacToeGame.showBoard(board);
-		int chance = (ticTacToeGame.toss()=="player")? 1:0;
+		
 		while(true) {
-			if (ticTacToeGame.emptySpace(board)) {
-				if (chance == 1) {
-					System.out.print("Player ");
-					board = ticTacToeGame.makeMove(board, player);
-					if (ticTacToeGame.winning(board,player)) {
-						System.out.println("Player won.");
-						break;
+			
+			Character[] board = ticTacToeGame.createBoard();
+			
+			char player = ticTacToeGame.chooseXorO();
+			char computer = ( player == 'X')? 'O':'X';
+			System.out.println("Player: "+player+" Computer: "+computer);
+			ticTacToeGame.showBoard(board);
+			
+			int chance = (ticTacToeGame.toss()=="player")? 1:0;
+			while(true) {
+				if (ticTacToeGame.emptySpace(board)) {
+					if (chance == 1) {
+						System.out.print("Player ");
+						board = ticTacToeGame.makeMove(board, player);
+						if (ticTacToeGame.winning(board,player)) {
+							System.out.println("Player won.");
+							break;
+						}
+						else {
+							//condition for changing turn.
+							chance = 0;
+						}
 					}
 					else {
-						//condition for changing turn.
-						chance = 0;
+						board = ticTacToeGame.computerMove(board, computer);
+						if (ticTacToeGame.winning(board,computer)) {
+							System.out.println("Computer won.");
+							break;
+						}
+						else {
+							chance = 1;
+						}
 					}
+					System.out.println();
+					ticTacToeGame.showBoard(board);
 				}
 				else {
-					board = ticTacToeGame.computerMove(board, computer);
-					if (ticTacToeGame.winning(board,computer)) {
-						System.out.println("Computer won.");
-						break;
-					}
-					else {
-						chance = 1;
-					}
+					System.out.println("Game Tie.");
+					break;
 				}
-				ticTacToeGame.showBoard(board);
+				
 			}
-			else {
-				System.out.println("Game Tie.");
-				break;
-			}
-			
+			System.out.println("Do you wish to play another game: 1.Yes 2.No");
+			if (sc.nextInt()==2) break;
 		}
 	}
 }
